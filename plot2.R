4c257078952934441702fc256b348c4b1f570d32
sm1992 <saurabhm0592@gmail.com>
2015-01-11 02:17:36 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
-data<-read.table("household_power_consumption.txt",header=TRUE,sep=';')
-png("myplot2.png",480,480)
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=';') #read data
+png("myplot2.png",480,480) #open device
 data2<-subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
 data2$DateTime <- as.POSIXct(paste(data2$Date, data2$Time), format="%d/%m/%Y %H:%M:%S")
 data2[,3]<-as.double(as.character(data2[,3]))
 plot(data2$DateTime,data2$Global_active_power,pch=' ',xlab='',ylab='Global Active Power (kilowatts)')
 lines(data2$DateTime,data2$Global_active_power)
-dev.off()
+dev.off()  #close and print plot as png
