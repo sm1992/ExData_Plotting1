90df3946f2b7d1d3df4aa521ad489123207ad6ad
sm1992 <saurabhm0592@gmail.com>
2015-01-11 02:20:03 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,7 +1,7 @@
-data<-read.table("household_power_consumption.txt",header=TRUE,sep=';')
-png("myplot4.png",480,480)
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=';') #read data
+png("myplot4.png",480,480) #open device
 data2<-subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
-data2$DateTime <- as.POSIXct(paste(data2$Date, data2$Time), format="%d/%m/%Y %H:%M:%S")
+data2$DateTime <- as.POSIXct(paste(data2$Date, data2$Time), format="%d/%m/%Y %H:%M:%S") #create datetime
 data2[,3]<-as.double(as.character(data2[,3]))
 par(mfrow=c(2,2))
 plot(data2[,10],data2[,3],pch=' ',ylab='Global Active Power')
@@ -20,4 +20,4 @@ legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('b
 data2[,4]<-as.double(as.character(data2[,4]))
 plot(data2[,10],data2[,4],pch=' ',ylab='Global_reactive_power',xlab='DateTime')
 lines(data2[,10],data2[,4])
-dev.off()
+dev.off() #close and print device
