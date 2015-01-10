d2dc8e54c914f7225c139e35df0b89d7261eb0bb
sm1992 <saurabhm0592@gmail.com>
2015-01-11 02:12:55 +0530

    new files

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data<-read.table("household_power_consumption.txt",header=TRUE,sep=';')
+png("myplot1.png",480,480)
+data2<-subset(data,data$Date=="1/2/2007" | data$Date=="2/2/2007")
+data2$DateTime <- as.POSIXct(paste(data2$Date, data2$Time), format="%d/%m/%Y %H:%M:%S")
+data2[,3]<-as.double(as.character(data2[,3]))
+hist(data2$Global_active_power,col="red",ylim=c(0,1200),xlim=c(0,6),xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
+dev.off()
